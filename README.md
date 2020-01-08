f1bd0ce899c66e8a2b47ab30848c14f3c933beb9
Ryan Shim <jhshim@robotis.com>
2020-01-08 15:04:34 +0900

    changed name pro -> p

--- README.md
+++ README.md
@@ -6,22 +6,22 @@
 
 |Version|Kinetic + Ubuntu Xenial|Melodic + Ubuntu Bionic|
 |:---:|:---:|:---:|
-|[![GitHub version](https://badge.fury.io/gh/ROBOTIS-GIT%2Fopen_manipulator_pro.svg)](https://badge.fury.io/gh/ROBOTIS-GIT%2Fopen_manipulator_pro)|[![Build Status](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_pro.svg?branch=kinetic-devel)](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_pro)|[![Build Status](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_pro.svg?branch=melodic-devel)](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_pro)|
+|[![GitHub version](https://badge.fury.io/gh/ROBOTIS-GIT%2Fopen_manipulator_p.svg)](https://badge.fury.io/gh/ROBOTIS-GIT%2Fopen_manipulator_p)|[![Build Status](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_p.svg?branch=kinetic-devel)](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_p)|[![Build Status](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_p.svg?branch=melodic-devel)](https://travis-ci.org/ROBOTIS-GIT/open_manipulator_p)|
 
 ## ROBOTIS e-Manual for OpenMANIPULATOR-PRO
 - [ROBOTIS e-Manual for OpenMANIPULATOR-PRO](http://emanual.robotis.com/docs/en/platform/openmanipulator_pro/overview/)
 
 ## Open Source related to OpenMANIPULATOR-PRO
 - [robotis_manipulator](https://github.com/ROBOTIS-GIT/robotis_manipulator)
-- [open_manipulator_pro](https://github.com/ROBOTIS-GIT/open_manipulator_pro)
-- [open_manipulator_pro_msgs](https://github.com/ROBOTIS-GIT/open_manipulator_pro_msgs)
-- [open_manipulator_pro_simulations](https://github.com/ROBOTIS-GIT/open_manipulator_pro_simulations)
+- [open_manipulator_p](https://github.com/ROBOTIS-GIT/open_manipulator_p)
+- [open_manipulator_msgs](https://github.com/ROBOTIS-GIT/open_manipulator_msgs)
+- [open_manipulator_p_simulations](https://github.com/ROBOTIS-GIT/open_manipulator_p_simulations)
 - [rh_p12_rn](https://github.com/ROBOTIS-GIT/RH-P12-RN)
 - [dynamixel_sdk](https://github.com/ROBOTIS-GIT/DynamixelSDK)
 - [dynamixel_workbench](https://github.com/ROBOTIS-GIT/dynamixel-workbench)
 
 ## Documents and Videos related to OpenMANIPULATOR-PRO
-- [ROBOTIS e-Manual for OpenMANIPULATOR-PRO](http://emanual.robotis.com/docs/en/platform/open_manipulator_pro/overview/)
+- [ROBOTIS e-Manual for OpenMANIPULATOR-PRO](http://emanual.robotis.com/docs/en/platform/open_manipulator_p/overview/)
 - [ROBOTIS e-Manual for RH-P12-RN](http://emanual.robotis.com/docs/en/platform/rh_p12_rn/)
 - [ROBOTIS e-Manual for Dynamixel SDK](http://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_sdk/overview/)
 - [ROBOTIS e-Manual for Dynamixel Workbench](http://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_workbench/)
---
+++ open_manipulator_p_control_gui/include/open_manipulator_p_control_gui/qnode.hpp
@@ -0,0 +1,144 @@
+﻿/*******************************************************************************
+* Copyright 2019 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Authors: Darby Lim, Hye-Jong KIM, Ryan Shim, Yong-Ho Na */
+
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef OPEN_MANIPULATOR_P_CONTROL_GUI_QNODE_HPP_
+#define OPEN_MANIPULATOR_P_CONTROL_GUI_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+// To workaround boost/qt4 problems that won't be bugfixed. Refer to
+//    https://bugreports.qt.io/browse/QTBUG-22829
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+#endif
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/String.h>
+
+#include <eigen3/Eigen/Eigen>
+
+#include "robotis_manipulator/robotis_manipulator.h"
+
+#include "open_manipulator_msgs/OpenManipulatorState.h"
+#include "open_manipulator_msgs/SetJointPosition.h"
+#include "open_manipulator_msgs/SetKinematicsPose.h"
+#include "open_manipulator_msgs/SetDrawingTrajectory.h"
+#include "open_manipulator_msgs/SetActuatorState.h"
+
+#define NUM_OF_JOINT_AND_TOOL 7
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace open_manipulator_p_control_gui {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+  void manipulatorStatesCallback(const open_manipulator_msgs::OpenManipulatorState::ConstPtr &msg);
+  void jointStatesCallback(const sensor_msgs::JointState::ConstPtr &msg);
+  void kinematicsPoseCallback(const open_manipulator_msgs::KinematicsPose::ConstPtr &msg);
+
+  std::vector<double> getPresentJointAngle();
+  std::vector<double> getPresentKinematicsPosition();
+  Eigen::Quaterniond getPresentKinematicsOrientation();
+  Eigen::Vector3d getPresentKinematicsOrientationRPY();
+  bool getOpenManipulatorMovingState();
+  bool getOpenManipulatorActuatorState();
+  bool getWithGripperState();
+
+  void setOption(std::string opt);
+  bool setJointSpacePath(std::vector<std::string> joint_name, std::vector<double> joint_angle, double path_time);
+  bool setTaskSpacePath(std::vector<double> kinematics_pose, double path_time);
+  bool setDrawingTrajectory(std::string name, std::vector<double> arg, double path_time);
+  bool setToolControl(std::vector<double> joint_angle);
+  bool setActuatorState(bool actuator_state);
+
+Q_SIGNALS:
+  void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+  QStringListModel logging_model;
+
+  // ROS Parameters
+  bool with_gripper_;
+
+  // ROS Publishers
+  ros::Publisher open_manipulator_option_pub_;
+
+  // ROS Subscribers
+  ros::Subscriber open_manipulator_states_sub_;
+  ros::Subscriber open_manipulator_joint_states_sub_;
+  ros::Subscriber open_manipulator_kinematics_pose_sub_;
+
+  // ROS Service Clients
+  ros::ServiceClient goal_joint_space_path_client_;
+  ros::ServiceClient goal_task_space_path_position_only_client_;
+  ros::ServiceClient goal_task_space_path_client_;
+  ros::ServiceClient goal_tool_control_client_;
+  ros::ServiceClient set_actuator_state_client_;
+  ros::ServiceClient goal_drawing_trajectory_client_;
+
+  std::vector<double> present_joint_angle_;
+  std::vector<double> present_kinematics_position_;
+  Eigen::Quaterniond present_kinematics_orientation_;
+  Eigen::Vector3d present_kinematics_orientation_rpy_;
+  open_manipulator_msgs::KinematicsPose kinematics_pose_;
+
+  bool open_manipulator_is_moving_;
+  bool open_manipulator_actuator_enabled_;
+};
+
+}  // namespace open_manipulator_p_control_gui
+
+#endif /* OPEN_MANIPULATOR_P_CONTROL_GUI_QNODE_HPP_ */
